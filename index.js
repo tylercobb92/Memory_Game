67da2ab6eadf0340cdb3fce6e86077f29b90ec71
tyler <tylercobb92@gmail.com>
2021-07-23 15:41:52 -0400

    cards change to class color when clicked; added classes of 'matched' and 'flipped' to css

--- index.js
+++ index.js
@@ -58,9 +58,10 @@ function createDivsForColors(colorArray) {
 }
 
 // TODO: Implement this function!
-function handleCardClick(event) {
-    // you can use event.target to see which element was clicked
-    console.log("you just clicked", event.target);
+function handleCardClick(e) {
+    // you can use e.target to see which element was clicked
+    console.log(e.target.className);
+    e.target.style.backgroundColor = e.target.className;
 }
 
 // when the DOM loads
